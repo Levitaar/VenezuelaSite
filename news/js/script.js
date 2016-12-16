a25239cd83d0978e1e6ed9bc809ef28d5af5bea5
Levitaar <luis.ghuiza@gmail.com>
2016-12-16 12:20:49 -0500

    Add files via upload

--- news/js/script.js
+++ news/js/script.js
@@ -1,3 +1,4 @@
+var stopwords = ["the", "by", "was"];
 var words = [];
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
@@ -18,7 +19,12 @@ $.ajax({
     var leadPara = result.response.docs[i].lead_paragraph.split(" ");
     for (var j = 0; j < leadPara.length; j++) {
       var test = leadPara[j].replace('"', "");
-      words.push(test);
+      var coma = test.replace(",", "");
+      var dot = coma.replace(".", "");
+      if(dot !== "the" && dot !== "was" && dot !== "THIS" && dot !== "it" && dot !== "he" && dot !== "with" && dot !== "and" && dot !== "of" && dot !== "a" && dot !== "has" && dot !== "in" && dot !== "that" && dot !== "THE" && dot !== "on" && dot !== "from" && dot !== "to" && dot !== "by" && dot !== "an") {
+        words.push(dot);
+        console.log(words);
+      }
     }
     }
 
@@ -27,14 +33,15 @@ $.ajax({
   for (var i = 0; i < words.length; i++) {
     if ($('*[data-word="' + words[i] + '"]').length > 0) {
       var target = $('*[data-word="' + words[i] + '"]');
-      target.css("font-size",target.data("fontsize")+10+"px");
+      target.css("font-size",target.data("fontsize")+30+"px");
       target.css("background-color","red");
+      target.css("transform","rotate(0deg)");
       console.log(target.data("fontsize"));
-      target.data("fontsize",(target.data("fontsize")+10));
+      target.data("fontsize",(target.data("fontsize")+30));
       
     } else {
       var target = $('*[data-word="' + words[i] + '"]');
-      $("body").append("<p data-word='" + words[i] + "' data-fontsize='20' style='font-size:20px; background-color: blue'>" + words[i] + "</p>");
+      $("body").append("<p data-word='" + words[i] + "' data-fontsize='20' style='font-size:30px; background-color: blue ; margin:5px ; display: inline-block; transform: rotate(90deg) ;'>" + words[i] + "</p>");
       
     }
 }
